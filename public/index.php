4283f7008421fbbe650513099b26995010331298
lelenaic <lenaic@lenaic.me>
2016-10-07 17:20:18 +0200

    Convert html templates in php + modify database connection (add options.php)

--- public/index.php
+++ public/index.php
@@ -5,14 +5,18 @@ require '../vendor/autoload.php';
 use Slim\Slim,
     RedBeanPHP\R;
 
-R::setup('mysql:host=149.202.160.19;dbname=hky16_v0', 'data', 'hk16');
+R::setup('mysql:host=' . __HOST__ . ';dbname='.__DB__, __USER__, __PASS__);
 
 $app=new Slim([
     'templates.path'=>'../templates/'
 ]);
 
-$app->get('/',function () use ($app){
-    $app->render('index.html');
+$app->get('/(:page)',function ($page='index') use ($app){
+    $app->render('includes/includes.php',compact('page'));
+});
+
+$app->post('/(:page)',function ($page='index') use ($app){
+    $app->render('includes/includes.php',compact('page'));
 });
 
 $app->run();
