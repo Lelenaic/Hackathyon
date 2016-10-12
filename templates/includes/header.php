20b2865860647d37986c29b25ed97f443f98de62
lelenaic <lenaic@lenaic.me>
2016-10-12 22:27:59 +0200

    Add weather functions

--- templates/includes/header.php
+++ templates/includes/header.php
@@ -89,115 +89,12 @@
                     <div class="left-bg"></div>
                 </li>
                 <li class="time">
-                    <h1 class="animated fadeInLeft">21:00</h1>
-                    <p class="animated fadeInRight">Sat,October 1st 2029</p>
+                    <h1 class="animated fadeInLeft"><?= date('H:i'); ?></h1>
+                    <p class="animated fadeInRight"><?= date('d/m/Y'); ?></p>
                 </li>
                 <li class="active ripple">
-                    <a class="tree-toggle nav-header"><span class="fa-home fa"></span> Dashboard
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                    <a class="tree-toggle nav-header" href="./"><span class="fa-home fa"></span> Accueil
                     </a>
-                    <ul class="nav nav-list tree">
-                        <li><a href="dashboard-v1">Dashboard v.1</a></li>
-                        <li><a href="dashboard-v2">Dashboard v.2</a></li>
-                    </ul>
-                </li>
-                <li class="ripple">
-                    <a class="tree-toggle nav-header">
-                        <span class="fa-diamond fa"></span> Layout
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
-                    </a>
-                    <ul class="nav nav-list tree">
-                        <li><a href="topnav">Top Navigation</a></li>
-                        <li><a href="boxed">Boxed</a></li>
-                    </ul>
-                </li>
-                <li class="ripple">
-                    <a class="tree-toggle nav-header">
-                        <span class="fa-area-chart fa"></span> Chart
-                        <span class="fa-angle-right fa right-arrow text-right"></span>
-                    </a>
-                    <ul class="nav nav-list tree">
-                        <li><a href="chartjs">ChartJs</a></li>
-                        <li><a href="morris">Morris</a></li>
-                        <li><a href="flot">Flot</a></li>
-                        <li><a href="sparkline">SparkLine</a></li>
-                    </ul>
-                </li>
-                <li class="ripple"><a class="tree-toggle nav-header">
-                        <span class="fa fa-pencil-square"></span> Ui Elements <span
-                            class="fa-angle-right fa right-arrow text-right"></span> </a>
-                    <ul class="nav nav-list tree">
-                        <li><a href="color">Color</a></li>
-                        <li><a href="weather">Weather</a></li>
-                        <li><a href="typography">Typography</a></li>
-                        <li><a href="icons">Icons</a></li>
-                        <li><a href="buttons">Buttons</a></li>
-                        <li><a href="media">Media</a></li>
-                        <li><a href="panels">Panels & Tabs</a></li>
-                        <li><a href="notifications">Notifications & Tooltip</a></li>
-                        <li><a href="badges">Badges & Label</a></li>
-                        <li><a href="progress">Progress</a></li>
-                        <li><a href="sliders">Sliders</a></li>
-                        <li><a href="timeline">Timeline</a></li>
-                        <li><a href="modal">Modals</a></li>
-                    </ul>
-                </li>
-                <li class="ripple"><a class="tree-toggle nav-header"><span class="fa fa-check-square-o"></span> Forms
-                        <span class="fa-angle-right fa right-arrow text-right"></span> </a>
-                    <ul class="nav nav-list tree">
-                        <li><a href="formelement.html">Form Element</a></li>
-                        <li><a href="">Wizard</a></li>
-                        <li><a href="#">File Upload</a></li>
-                        <li><a href="#">Text Editor</a></li>
-                    </ul>
-                </li>
-                <li class="ripple"><a class="tree-toggle nav-header"><span class="fa fa-table"></span> Tables <span
-                            class="fa-angle-right fa right-arrow text-right"></span> </a>
-                    <ul class="nav nav-list tree">
-                        <li><a href="datatables">Data Tables</a></li>
-                        <li><a href="handsontable">handsontable</a></li>
-                        <li><a href="tablestatic">Static</a></li>
-                    </ul>
-                </li>
-                <li class="ripple"><a href="calendar"><span class="fa fa-calendar-o"></span>Calendar</a></li>
-                <li class="ripple"><a class="tree-toggle nav-header"><span class="fa fa-envelope-o"></span> Mail <span
-                            class="fa-angle-right fa right-arrow text-right"></span> </a>
-                    <ul class="nav nav-list tree">
-                        <li><a href="mail-box">Inbox</a></li>
-                        <li><a href="compose-mail">Compose Mail</a></li>
-                        <li><a href="view-mail">View Mail</a></li>
-                    </ul>
-                </li>
-                <li class="ripple"><a class="tree-toggle nav-header"><span class="fa fa-file-code-o"></span> Pages <span
-                            class="fa-angle-right fa right-arrow text-right"></span> </a>
-                    <ul class="nav nav-list tree">
-                        <li><a href="forgotpass">Forgot Password</a></li>
-                        <li><a href="login">SignIn</a></li>
-                        <li><a href="reg">SignUp</a></li>
-                        <li><a href="article-v1">Article v1</a></li>
-                        <li><a href="search-v1">Search Result v1</a></li>
-                        <li><a href="productgrid">Product Grid</a></li>
-                        <li><a href="profile-v1">Profile v1</a></li>
-                        <li><a href="invoice-v1">Invoice v1</a></li>
-                    </ul>
-                </li>
-                <li class="ripple"><a class="tree-toggle nav-header"><span class="fa "></span> MultiLevel <span
-                            class="fa-angle-right fa right-arrow text-right"></span> </a>
-                    <ul class="nav nav-list tree">
-                        <li><a href="view-mail">Level 1</a></li>
-                        <li><a href="view-mail">Level 1</a></li>
-                        <li class="ripple">
-                            <a class="sub-tree-toggle nav-header">
-                                <span class="fa fa-envelope-o"></span> Level 1
-                                <span class="fa-angle-right fa right-arrow text-right"></span>
-                            </a>
-                            <ul class="nav nav-list sub-tree">
-                                <li><a href="mail-box">Level 2</a></li>
-                                <li><a href="compose-mail">Level 2</a></li>
-                                <li><a href="view-mail">Level 2</a></li>
-                            </ul>
-                        </li>
-                    </ul>
                 </li>
                 <li><a href="credits">Credits</a></li>
             </ul>
